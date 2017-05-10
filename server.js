67fd69a146aa1dc5a96f9fb49140b5d25b531cc5
Ted Ross <tross@redhat.com>
2017-05-10 14:27:58 -0400

    try toSource

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/pagecount', function (req, res) {
 });
 
 app.get('/proxy', function (req, res) {
-    res.send('Pending Proxy Function - from ' + JSON.stringify(req, null, 4));
+    res.send('Pending Proxy Function - from ' + req.toSource());
 });
 
 // error handling
